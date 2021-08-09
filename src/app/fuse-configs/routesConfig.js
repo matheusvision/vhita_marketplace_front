e2447c1bd694440bab905f29e3fdb3bc6d13bd32
mustafahlvc <mustafahlvc@gmail.com>
2021-08-09 12:33:21 +0300

    404 page added.

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -2,6 +2,7 @@ import { Redirect } from 'react-router-dom';
 import FuseUtils from '@fuse/utils';
 import ExampleConfig from 'app/main/example/ExampleConfig';
 import FuseLoading from '@fuse/core/FuseLoading';
+import Error404Page from 'app/main/404/Error404Page';
 
 const routeConfigs = [ExampleConfig];
 
@@ -11,6 +12,7 @@ const routes = [
   // The individual route configs which has auth option won't be overridden.
   ...FuseUtils.generateRoutesFromConfigs(routeConfigs, null),
   {
+    exact: true,
     path: '/',
     component: () => <Redirect to="/example" />,
   },
@@ -18,6 +20,13 @@ const routes = [
     path: '/loading',
     exact: true,
     component: () => <FuseLoading />,
+  },
+  {
+    path: '/404',
+    component: () => <Error404Page />,
+  },
+  {
+    component: () => <Redirect to="/404" />,
   },
 ];
 
