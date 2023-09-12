e07583248030a86494f12fa2fe166cb73bb9ddb3
mustafa <mustafahlvc@gmail.com>
2023-09-12 14:48:13 +0300

    NavLinkAdapter updated.

--- src/@fuse/core/NavLinkAdapter/NavLinkAdapter.tsx
+++ src/@fuse/core/NavLinkAdapter/NavLinkAdapter.tsx
@@ -1,23 +1,27 @@
 import { CSSProperties, forwardRef } from 'react';
 import { NavLink as BaseNavLink, NavLinkProps as BaseNavLinkProps } from 'react-router-dom';
 
-type NavLinkProps = BaseNavLinkProps & {
+export type NavLinkAdapterPropsType = BaseNavLinkProps & {
 	activeClassName?: string;
 	activeStyle?: CSSProperties;
 };
 
-const NavLinkAdapter = forwardRef<HTMLAnchorElement, NavLinkProps>(
-	({ activeClassName = 'active', activeStyle, ...props }: NavLinkProps, ref) => (
+const NavLinkAdapter = forwardRef<HTMLAnchorElement, NavLinkAdapterPropsType>((props, ref) => {
+	const { activeClassName = 'active', activeStyle, ..._props } = props;
+
+	return (
 		<BaseNavLink
 			ref={ref}
-			{...props}
-			className={({ isActive }) => [props.className, isActive ? activeClassName : null].filter(Boolean).join(' ')}
+			{..._props}
+			className={({ isActive }) =>
+				[_props.className, isActive ? activeClassName : null].filter(Boolean).join(' ')
+			}
 			style={({ isActive }) => ({
-				...props.style,
+				..._props.style,
 				...(isActive ? activeStyle : null)
 			})}
 		/>
-	)
-);
+	);
+});
 
 export default NavLinkAdapter;
