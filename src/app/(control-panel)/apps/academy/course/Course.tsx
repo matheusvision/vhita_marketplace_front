d7adb0f89b9011ab603aacb14e83af92e18db12e
mustafa <mustafahlvc@gmail.com>
2024-12-07 05:24:55 +0300

    update eslint config, use flat eslint config, fix eslint rules.

--- src/app/(control-panel)/apps/academy/course/Course.tsx
+++ src/app/(control-panel)/apps/academy/course/Course.tsx
@@ -58,6 +58,7 @@ function Course() {
 				data: { ...course, progress: { currentStep: 1, completed: 0 } }
 			});
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [course]);
 
 	useEffect(() => {
